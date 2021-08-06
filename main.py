b2a0827258c70a2a1d27082358ae26c43d2852ec
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-05 20:10:08 -0700

    gary

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+    string = "This is a string!"
+    integer = 8
+    bool = True
+    float = 5.2
+
+    #print(string)
+
+
+
+    username = input("enter a user:\n")
+    age = int(input("enter your age:\n"))
+
+    #print("Hi!!!  My favorite number is " + integer)
+
+    print(username + " is " + str(age) + " years old")
+
+    number = "11"
+    number = int(number)
+
+
+
+
+if __name__ == '__main__':
+    main()
